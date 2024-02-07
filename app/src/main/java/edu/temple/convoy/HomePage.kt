df89cf427fecf3380634d0ce664ecfb1e9ad7693
Nicholas Rucinski <tur45021@temple.edu>
2024-02-07 10:01:50 -0500

    got a simple navigation and login screen created

---
+++ app/src/main/java/edu/temple/convoy/HomePage.kt
@@ -0,0 +1,12 @@
+package edu.temple.convoy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page)
+
+    }
+}
--- app/src/main/java/edu/temple/convoy/MainActivity.kt
+++ app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -1,16 +1,50 @@
 package edu.temple.convoy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    var loginVerified = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var isFirstTime = true
+        if(!isFirstTime){
+            launchHomePage()
+        }
 
         findViewById<Button>(R.id.createAccountButton).setOnClickListener{
-            setContentView(R.layout.home_page)
+            createAccountSetup()
         }
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            loginSetup()
+        }
+
+    }
+
+    fun createAccountSetup(){
+        setContentView(R.layout.create_account_screen)
+        findViewById<Button>(R.id.submitButton).setOnClickListener {
+            if(loginVerified){
+                launchHomePage()
+            }
+        }
+    }
+
+    fun loginSetup(){
+        setContentView(R.layout.login_screen)
+        findViewById<Button>(R.id.submitButton).setOnClickListener {
+            if(loginVerified){
+                launchHomePage()
+            }
+        }
+    }
+
+    fun launchHomePage(){
+        startActivity(Intent(this, HomePage::class.java))
     }
 }
